3915541f51c7893aff4f3524450bdd9525e94874
Eelco Rouw (Pi3) <eelco.rouw@gmail.com>
2013-06-30 21:34:23 +0200

    Additional test service for 10 random numbers

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import web, json
+from math import *
 
-urls = ('/', 'index', '/main.json','main')
+urls = ('/', 'index', '/main.json','main', '/sin.json', 'sin')
 
 app = web.application(urls, globals())
 render = web.template.render('templates/')
@@ -12,6 +13,15 @@ class main:
 { "x_axis": 70, "y_axis": 70, "height": 20, "width":20, "color" : "red", "label" : "three" }]
 		web.header('Content-Type', 'application/json')
 		return json.dumps(a)
+
+class sin:
+	def GET(self):
+		a = []
+		for i in range(0,10):
+			a.append({'xvalue':random.random()})
+		web.header('Content-Type', 'application/json')
+		return json.dumps(a)	
+		
 
 class index:
 	def GET(self):
