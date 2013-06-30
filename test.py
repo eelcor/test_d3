f481410c7d4ef81381bf78c14ea22734e4e18012
Eelco Rouw (Pi3) <eelco.rouw@gmail.com>
2013-06-30 19:09:28 +0200

    Een werkende webservice op basis van Web2Py en json

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-import web
+import web, json
 
-urls = ('/','main')
+urls = ('/', 'index', '/main.json','main')
 
 app = web.application(urls, globals())
+render = web.template.render('templates/')
 
 class main:
 	def GET(self):
-		return 'Hello wereld!'
+		a = [{ "x_axis": 10, "y_axis": 10, "height": 20, "width":20, "color" : "green", "label" : "one" },
+{ "x_axis": 40, "y_axis": 40, "height": 20, "width":20, "color" : "purple", "label" : "two" },
+{ "x_axis": 70, "y_axis": 70, "height": 20, "width":20, "color" : "red", "label" : "three" }]
+		web.header('Content-Type', 'application/json')
+		return json.dumps(a)
+
+class index:
+	def GET(self):
+		return render.index()
 
 if __name__ == "__main__":
 	app.run()
